b5b700aecb363bfb0dee84237ad6bdcf284d2e1b
Miranto <hei.finoana.5@gmail.com>
2026-03-12 16:15:55 +0300

    feat : add booking creation endpoint

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from typing import List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Json
 from datetime import date
+
+from starlette.responses import JSONResponse
+
 app = FastAPI()
 
-reservation = []
+bookings = []
 
-class Reservation(BaseModel):
+class Booking(BaseModel):
     customer_name : str
     customer_phone : str
     customer_email : str
@@ -13,10 +17,21 @@ class Reservation(BaseModel):
     reservation_description : str
     reservation_date : date
 
-@app.get("/booking")
+@app.get("/booking",response_model=List[Booking],status_code=200)
 def get_reservation():
-    return {
-        "message": "List of reservation",
-        "data": reservation
-    }
+    return bookings
+
+@app.post("/booking",response_model=List[Booking],status_code=200)
+def create_reservation(booking: Booking):
+    for existing_booking in bookings:
+        if(
+            existing_booking.room_number == booking.room_number
+            and existing_booking.reservation_date == booking.reservation_date
+        ):
+            return JSONResponse(
+                status_code=409,
+                content={"message": "This room is not available for this date"}
+            )
+    bookings.append(booking)
+    return bookings
 
